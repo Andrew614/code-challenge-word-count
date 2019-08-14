bb88e2330eb70628e7deb15a6df3fd457c11fbed
Andrew614 <azhang213@gmail.com>
2019-08-14 18:24:39 -0400

    able to read and parse a text file

--- src/WordCount.java
+++ src/WordCount.java
@@ -1,7 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class WordCount {
 
 	public static void main(String[] args) {
-
+		try {
+			File importFile = new File("Paragraph.txt");
+			Scanner file = new Scanner(importFile);
+			
+			while (file.hasNext()) {
+				String word = file.next();
+				System.out.println(word);
+			}
+			file.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("The file does not exist");
+			e.printStackTrace();
+		}
 	}
 
 }
