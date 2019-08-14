0826c98a80c7fb24cd39eab111e724275a39bd42
Andrew614 <azhang213@gmail.com>
2019-08-14 18:47:33 -0400

    count occurrence of each word in the text file

--- src/WordCount.java
+++ src/WordCount.java
@@ -1,22 +1,48 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class WordCount {
 
 	public static void main(String[] args) {
 		try {
+			Map<String, Integer> wordCountData = new HashMap<String, Integer>();
 			File importFile = new File("Paragraph.txt");
 			Scanner file = new Scanner(importFile);
+			countWords(file, wordCountData);
 			
-			while (file.hasNext()) {
-				String word = file.next();
-				System.out.println(word);
-			}
+			System.out.println(wordCountData);
 			file.close();
+			
 		} catch (FileNotFoundException e) {
 			System.out.println("The file does not exist");
 			e.printStackTrace();
+		}
+	}
+
+	
+	private static void countWords(Scanner file, Map<String, Integer> wordCountData) {
+		List<String> wordsList = new ArrayList<String>();
+		Set<String> noRepeatList = new HashSet<String>();
+		while (file.hasNext()) {
+			String word = file.next();
+			wordsList.add(word);
+			noRepeatList.add(word);
+		}
+		for (String noRepeatListWord : noRepeatList) {
+			int wordCount = 0;
+			for (String wordsListWord : wordsList) {
+				if (noRepeatListWord.equals(wordsListWord)) {
+					wordCount++;
+				}
+			}
+			wordCountData.put(noRepeatListWord, wordCount);
 		}
 	}
 
